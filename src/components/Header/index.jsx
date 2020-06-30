3fb02113559160920c19da5211b77755ca92b120
Ursula Kesek <ursky@blueyonder.co.uk>
2020-06-30 22:53:12 +0100

    edited header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,6 +6,14 @@ const Header = () => (
   <>
     <div className="header">
       <img className="blue" src={BlueCat} alt="blue-cat" />
+      <h1>UK PERFUMES</h1>
+      <div class="box1">Box1</div>
+      <div class="box2">Box2</div>
+      <div class="box3">Box3</div>
+      <div class="box4">Box4</div>
+      <div class="box5">Box5</div>
+      <div class="box6">Box6</div>
+
     </div>
   </>
 );
