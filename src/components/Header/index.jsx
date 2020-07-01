62edec04242fb213ab6120b8349c11c7921c7e18
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-01 15:57:31 +0100

    edited header index and css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,10 +8,10 @@ const Header = () => (
     <div className="header">
       <img className="blue" src={BlueCat} alt="blue-cat" />
       <h1>UK PERFUMES</h1>
-      <div className="box1"></div>
-      <div>
+      <div className="box1">
+      <p className="exotic">Travel to exotic places through scents</p>
       <img className="pb75" src={pb75} alt="small-squat-perfume-bottle" />
-    </div>
+      </div>
     </div>
   </>
 );
