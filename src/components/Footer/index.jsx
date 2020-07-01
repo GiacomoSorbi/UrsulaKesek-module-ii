ecb61cf542dd8c7e6e01525e6babc7e19b6978fd
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-01 19:57:05 +0100

    edited footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import "./Footer.css";
 
 const Footer=()=>(
-    <div className="footer">This is the Footer</div>
+    <div className="footer">
+        This is the Footer
+        </div>
 
 )
 export default Footer;
