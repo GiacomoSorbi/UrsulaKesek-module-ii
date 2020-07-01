4b188e0cc054c8e36090ec889829a2c6281e928c
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-01 12:54:21 +0100

    edited navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
-
+import clubs from "./Images/clubs1.png";
 const NavBar = () => (
   <nav>
     <ul>
+    <img className="clubs" src={clubs} alt="blue-cat" />
       <li>
         <Link className="home" to="/" >
           Home
         </Link>
+        <img className="clubs" src={clubs} alt="blue-cat" />
       </li>
       <li>
         <Link className="shop" to="/shop">
           Shop
         </Link>
+        <img className="clubs" src={clubs} alt="blue-cat" />
       </li>
       <li>
         <Link className="plants" to="/plants">
           Plants
         </Link>
+        <img className="clubs" src={clubs} alt="blue-cat" />
       </li>
       <li>
         <Link className="about" to="/about">
           About
         </Link>
+        <img className="clubs" src={clubs} alt="blue-cat" />
       </li>
     </ul>
   </nav>
