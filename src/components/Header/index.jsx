99f557a1129c58f95a919db07b61bb2262e48562
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-01 17:54:42 +0100

    added 2 images

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./Header.css";
-import BlueCat from "./Images/blue-cat-150.png";
+import GreenCat from "./Images/green-cat.png";
 import pb75 from "./Images/pb75.png";
-
 const Header = () => (
   <>
     <div className="header">
-      <img className="blue" src={BlueCat} alt="blue-cat" />
+      <img className="blue" src={GreenCat} alt="blue-cat" />
       <h1>UK PERFUMES</h1>
       <div className="box1">
-      <p className="exotic">Travel to exotic places through scents</p>
+      <p className="exotic">Travel places through scents</p>
       <img className="pb75" src={pb75} alt="small-squat-perfume-bottle" />
       </div>
     </div>
