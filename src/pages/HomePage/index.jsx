bc0fe624dd861a7ba8f2d75e3cfa8ad0eb5647e6
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-02 16:47:03 +0100

    edited the homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import Front from "./Images/aw-front.png";
 
-const HomePage = () => <div>I am the Home Page</div>;
-
+function HomePage () {
+    return(
+        <section className="main">
+        <div className="box2">
+        <h2>Scent Families </h2>
+          <div className="perfOne">
+          <p>Floral</p>
+          </div>
+          <div className="perfTwo">
+          <p>Fresh</p>
+          </div>
+          <div className="perfThree">
+          <p>Oriental</p>
+          </div>
+          <div className="perfFour">
+          <p>Woody</p>
+          </div>
+          </div>
+        <img className="front" src={Front} alt="andy-window" />
+        <div className="box3"></div>
+      </section>
+        )
+}
 export default HomePage;
