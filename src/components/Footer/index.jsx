12386082a7dbc342e6960a7db19224735b5c55f8
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-03 16:57:50 +0100

    edited social media icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import "./Footer.css";
-import Behance from "./Images/Behance.svg";
-import Deviant from "./Images/Deviantart.svg";
-import Dribbble from "./Images/Dribbble.svg";
-import Github from "./Images/Github.svg";
-import Pinterest from "./Images/Pinterest.svg";
-import Snapchat from "./Images/Snapchat.svg";
-import Twitter from "./Images/Twitter.svg";
-import WhatsApp from "./Images/WhatsApp.svg";
-import YouTube from "./Images/YouTube.svg";
+import Behance from "./Images/Behance.png";
+import Deviant from "./Images/Deviantart.png";
+import Dribbble from "./Images/Dribbble.png";
+import Facebook from "./Images/Facebook.png";
+import Github from "./Images/Github.png";
+import Reddit from "./Images/Reddit.png";
+import Snapchat from "./Images/Snapchat.png";
+import Twitter from "./Images/Twitter.png";
 
 
 const Footer=()=>(
-    <div className="footer">
+    <footer className="footer">
+      <div className="sm">
       <img className="beh" src={Behance} alt="behance-logo" />
       <img className="dev" src={Deviant} alt="deviantart-logo" />
       <img className="dri" src={Dribbble} alt="dribbble-logo" />
+      <img className="fac" src={Facebook} alt="facebook-logo" />
       <img className="git" src={Github} alt="github-logo" />
-      <img className="pin" src={Pinterest} alt="pinterest-logo" />
+      <img className="red" src={Reddit} alt="reddit-logo" />
       <img className="sna" src={Snapchat} alt="snapchat-logo" />
       <img className="twi" src={Twitter} alt="twitter-logo" />
-      <img className="wha" src={WhatsApp} alt="whatsapp-logo" />
-      <img className="you" src={YouTube} alt="youtube-logo" />
-
       </div>
+      </footer>
 
 )
 export default Footer;
