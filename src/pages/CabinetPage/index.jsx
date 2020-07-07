a9bd7c68f394e427cb2b9bb8e675fa6f8d3d285d
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-07 18:55:55 +0100

    added the wheel

--- src/pages/CabinetPage/index.jsx
+++ src/pages/CabinetPage/index.jsx
@@ -1,40 +1,40 @@
 import React from "react";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./Cabinet.css";
+import fragranceWheel from "./Images/fragrance-wheel.png";
 
-const CabinetPage = () =>
-<>
- <div>
-    <div className="box2">
+const CabinetPage = () => (
+  <>
+    <section className="aroma">
+      <main className="box2">
         <h2>Cabinet Of Curiosities </h2>
-        <hr className="new1"/>
         <div className="perfOne">
           <p className="fl">Floral</p>
           <NavLink className="floral" to="/floral">
             A
-           </NavLink>
-           <NavLink className="floral" to="/floral">
+          </NavLink>
+          <NavLink className="floral" to="/floral">
             B
-            </NavLink>
-            <NavLink className="floral" to="/floral">
+          </NavLink>
+          <NavLink className="floral" to="/floral">
             C
-            </NavLink>
+          </NavLink>
         </div>
         <div className="perfTwo">
-        <p className="fr">Fresh</p>
+          <p className="fr">Fresh</p>
 
           <NavLink className="fresh" to="/fresh">
-           D
+            D
           </NavLink>
           <NavLink className="fresh" to="/fresh">
-           E
+            E
           </NavLink>
           <NavLink className="fresh" to="/fresh">
-           F
+            F
           </NavLink>
         </div>
         <div className="perfThree">
-        <p className="or">Oriental</p>
+          <p className="or">Oriental</p>
           <NavLink className="orient" to="/oriental">
             G
           </NavLink>
@@ -46,7 +46,7 @@ const CabinetPage = () =>
           </NavLink>
         </div>
         <div className="perfFour">
-        <p className="wo">Woody</p>
+          <p className="wo">Woody</p>
           <NavLink className="woody" to="/woody">
             K
           </NavLink>
@@ -57,8 +57,12 @@ const CabinetPage = () =>
             M
           </NavLink>
         </div>
-    </div>
-    </div>
-    </>
-    
+      </main>
+      <aside classname="wheel">
+        <img className="sntwl" src={fragranceWheel} alt="perfume-chart" />
+      </aside>
+    </section>
+  </>
+);
+
 export default CabinetPage;
