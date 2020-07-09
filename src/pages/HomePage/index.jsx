0bbf89aa62c5d3c63fd4352c3bd8228134441ef8
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-09 13:46:15 +0100

    added bird gif

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,14 +3,16 @@ import Front from "./Images/aw-front.png";
 import Bubble from "./Images/bubble-iv.gif";
 import "./HomePage.css";
 import GreenCat from "./Images/green-cat.png";
-
+import Bird from "./Images/bird200x200.gif";
 
 function HomePage() {
   return (
     <section className="main">
       <div className="box2">
         <h2 className="hph2">COOL CATS AND CHICKS ROAM THE WORLD</h2>
+        <img className="bird" src={Bird} alt="robin-bird" />
         <img className="green1" src={GreenCat} alt="green-cat" />
+
       </div>
       <img className="front" src={Front} alt="andy-window" />
       <div className="box3">
