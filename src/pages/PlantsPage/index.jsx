8863ca6b30e7ba0694ef5654a8bf75f1532613ac
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-15 00:12:17 +0100

    edited several css's

--- src/pages/PlantsPage/index.jsx
+++ src/pages/PlantsPage/index.jsx
@@ -3,6 +3,11 @@ import "./PlantsPage.css";
 const PlantsPage = () => (
   <main className="plpg">
     <h2>I am the Plants page!</h2>
+    <section className="plantsec">
+      <p className='plaa'>A </p>
+      <p className="plab">B</p>
+      <p className="plac">C</p>
+    </section>
   </main>
 );
 export default PlantsPage;
