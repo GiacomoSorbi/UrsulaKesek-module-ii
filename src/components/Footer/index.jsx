3e52fb9865be04d3c535ceb7bc0e41fa99920fab
Ursula Kesek <ursky@blueyonder.co.uk>
2020-07-17 12:17:02 +0100

    edited social media

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -14,14 +14,14 @@ const Footer=()=>(
     <footer className="footer">
       <p>Ursula Kesek made this website in Hackney in July 2020.</p>
       <div className="sm">
-      <img className="beh" src={Behance} alt="behance-logo" />
-      <img className="dev" src={Deviant} alt="deviantart-logo" />
-      <img className="dri" src={Dribbble} alt="dribbble-logo" />
-      <img className="fac" src={Facebook} alt="facebook-logo" />
-      <img className="git" src={Github} alt="github-logo" />
-      <img className="red" src={Reddit} alt="reddit-logo" />
-      <img className="sna" src={Snapchat} alt="snapchat-logo" />
-      <img className="twi" src={Twitter} alt="twitter-logo" />
+      <img className="beh" src={Behance} alt="behance-logo" a href= "https://www.behance.net/"target="_blank" rel="noopener noreferrer"/>
+      <img className="dev" src={Deviant} alt="deviantart-logo" a href="https://www.deviantart.com/"target="_blank" rel="noopener noreferrer" />
+      <img className="dri" src={Dribbble} alt="dribbble-logo" a href="https://dribbble.com/"target="_blank" rel="noopener noreferrer"/>
+      <img className="fac" src={Facebook} alt="facebook-logo" a href="https://en-gb.facebook.com/"target="_blank" rel="noopener noreferrer" />
+      <img className="git" src={Github} alt="github-logo" a href="https://github.com/"target="_blank" rel="noopener noreferrer"/>
+      <img className="red" src={Reddit} alt="reddit-logo" a href="https://www.reddit.com/" target="_blank" rel="noopener noreferrer"/>
+      <img className="sna" src={Snapchat} alt="snapchat-logo" a href="https://www.snapchat.com/"target="_blank" rel="noopener noreferrer" />
+      <img className="twi" src={Twitter} alt="twitter-logo" a href="https://twitter.com/search-home?lang=en-gb"target="_blank" rel="noopener noreferrer" />
       </div>
       </footer>
 
