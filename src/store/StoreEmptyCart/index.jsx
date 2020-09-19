0783207a3ebc1f8c7efa61a41b84688b0155e724
Ursula Kesek <ursky@blueyonder.co.uk>
2020-09-19 10:19:36 +0100

    edited several pages

--- src/store/StoreEmptyCart/index.jsx
+++ src/store/StoreEmptyCart/index.jsx
@@ -5,8 +5,8 @@ const StoreEmptyCart=()=>{
     return(
         <section className="emptycart">
             <h1>There are no items in your cart.</h1>
-            <h2>Take another look:Click on  SHOP and add items! </h2>
-            <h2>Or check the weather in the Cities mentioned:Click on WEATHER.</h2>
+            <h2 className="ecc">Take another look:Click on  SHOP and add items! </h2>
+            <h2 className="ecw">Or check the weather in the Cities mentioned:Click on WEATHER.</h2>
         </section>
     )
 }
