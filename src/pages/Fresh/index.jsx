ed8bc493b20606acfa46a53504ad5d4de8dc7b98
Ursula Kesek <ursky@blueyonder.co.uk>
2020-09-23 01:37:55 +0100

    edited fresh images

--- src/pages/Fresh/index.jsx
+++ src/pages/Fresh/index.jsx
@@ -7,19 +7,19 @@ const Fresh = () => (
       <section className="fresec">
         <div>
         <h2 className="olfa">Fruity</h2>
-        <h3 className="floa">D</h3>
+        <h3 className="fred">D</h3>
         </div>
         <div>
         <h2 className="olfa">Green</h2>
-        <h3 className="floa">E</h3>
+        <h3 className="free">E</h3>
         </div>
         <div>
         <h2 className="olfa">Water</h2>
-        <h3 className="floa">F</h3>
+        <h3 className="fref">F</h3>
         </div>
         <div>
         <h2 className="olfa">Citrus</h2>
-        <h3 className="floa">G</h3>
+        <h3 className="freg">G</h3>
         </div>
 
       </section>
