170dccc22eab5174299885978c80be553f5a850a
Ursula Kesek <ursky@blueyonder.co.uk>
2020-09-25 17:41:41 +0100

    edited for responsive css

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -11,23 +11,25 @@ import "./HomePage.css";
 
 function HomePage() {
   return (
-    <section className="main">
-      <div className="box2">
-        <h2 className="hph2">COOL CHICKS AND CATS ROAM THE WORLD</h2>
-        <img className="bird" src={Bird} alt="robin-bird" />
-        <img className="green1" src={GreenCat} alt="green-cat" />
-        <NavLink className="contact1" to="/contact">
-          YOUR CONTACT DETAILS
-        </NavLink>
-        <img className="chick" src={Chick} alt="small-robin-bird" />
-        <img className="blue1" src={BlueCat} alt="blue-cat" />
-      </div>
-      <img className="front" src={Front} alt="andy-window" />
-      <img className="front2" src={Front2} alt="andy-window2" />
-      <div className="box3">
-        <img className="bubble" src={Bubble} alt="small-bubble-animation" />
-      </div>
-    </section>
+    <>
+      <section className="main">
+        <div className="box2">
+          <h2 className="hph2">COOL CHICKS AND CATS ROAM THE WORLD</h2>
+          <img className="bird" src={Bird} alt="robin-bird" />
+          <img className="green1" src={GreenCat} alt="green-cat" />
+          <NavLink className="contact1" to="/contact">
+            YOUR CONTACT DETAILS
+          </NavLink>
+          <img className="chick" src={Chick} alt="small-robin-bird" />
+          <img className="blue1" src={BlueCat} alt="blue-cat" />
+        </div>
+        <img className="front" src={Front} alt="andy-window" />
+        <img className="front2" src={Front2} alt="andy-window2" />
+        <div className="box3">
+          <img className="bubble" src={Bubble} alt="small-bubble-animation" />
+        </div>
+      </section>
+    </>
   );
 }
 export default HomePage;
